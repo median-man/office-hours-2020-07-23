ff2ce43b075b0e3ba99c20dbccef9cdd885bd6a1
John Desrosiers <john.desro@gmail.com>
2020-07-23 09:44:45 -0700

    Add dotenv and hide server secret env var

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # office-hours-2020-07-23
 Securing API keys demo with dotenv
+
+## Setup
+
+The following environmental variables must be provided. Example .env file:
+
+```
+SERVER_SECRET = 123456
+```
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
@@ -15,7 +16,11 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: process.env.SERVER_SECRET, resave: true, saveUninitialized: true })
+  session({
+    secret: process.env.SERVER_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
